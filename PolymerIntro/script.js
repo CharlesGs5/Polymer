1e9fc9a9bd87aa003336994a4b262f9b1ce14131
CharlesGs5 <carlosgs125@gmail.com>
2019-05-30 15:36:33 -0500

    Primer ejercicio terminado.

--- PolymerIntro/script.js
+++ PolymerIntro/script.js
@@ -1,7 +1,4 @@
 const arrayElements = [[1,2],[2,3,4],[5,6]];
-const newArray = arrayElements.reduce((total, value) => {
-    return total.concat(value);
-}, []);
+const newArray = [...new Set(arrayElements.reduce((total, value) => total.concat(value), []))];
 console.log(newArray);
 
-console.log("De nuevo")
