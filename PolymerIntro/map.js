7bc68ef8a0ea915fcc612cd71b1419d50eb96b71
CharlesGs5 <carlosgs125@gmail.com>
2019-05-31 18:00:18 -0500

    Se agregaron notas y parte de la segunda tarea.

---
+++ PolymerIntro/map.js
@@ -0,0 +1,14 @@
+const latLong = [
+    [48.8667, 2.33333],
+    [19.4978, -99.1269],
+    [40.4167, -3.70325],
+    [40.6643, -73.9385],
+    [37.5657, 126.978]
+];
+
+const cities = ['Paris', 'México', 'New York', 'Madrid', 'Seúl'];
+
+const address = new Map([...latLong]);
+
+console.log(address);
+
